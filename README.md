c99a9ad40edc5622cd8c1ae887bc8932405ee07d
Assaf <assaf@localhost>
2014-06-04 18:25:04 -0700

    add pure java tests and maven profile to run all non-selenium tests

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Selenium_test
 =============
 mvn -Dselenium.server.url=http://10.152.32.25:4444/wd/hub -Dselenium.test.url.SimpleTest=http://10.152.32.35:8081/bank/main clean test
 
+Use -Pwithout-selenium to run only local tests
+
---
+++ src/test/java/com/vmware/test/JavaTest2.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2014 VMware, Inc.  All rights reserved. -- VMware Confidential
+ */
+package com.vmware.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ *
+ */
+public class JavaTest2 {
+    @Test
+    public void test() {
+        assertTrue("Oh, oh...", true);
+    }
+}
